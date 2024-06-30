dca3aa744db7a26923126344a57f8f229e589ce2
Rui Fernando Hayashi <rfhayashi@gmail.com>
2024-06-30 10:56:00 -0300

    remove functions

--- web/spider.js
+++ web/spider.js
@@ -1,46 +1,16 @@
 (() => {
-  function gotoUrl(params) {
-    let {url} = params;
-    window.location = url;
-  }
-
-  function scrollDown() {
-    window.scrollByLines(1);
-  }
-
-  function scrollUp() {
-    window.scrollByLines(-1);
-  }
-
-  function scrollTop() {
-    window.scroll(0, 0);
-  }
-
-  function scrollBottom() {
-    window.scroll(0, document.body.scrollHeight);
-  }
-
-  function goBack() {
-    history.back();
-  }
-
-  function goForward() {
-    history.forward();
-  }
-
-  function reloadPage() {
-    location.reload();
-  }
-
   let commands = {
-    "goto-url": gotoUrl,
-    "scroll-down": scrollDown,
-    "scroll-up": scrollUp,
-    "scroll-top": scrollTop,
-    "scroll-bottom": scrollBottom,
-    "go-back": goBack,
-    "go-forward": goForward,
-    "reload-page": reloadPage
+    "goto-url": (params) => {
+      let {url} = params;
+      window.location = url;
+    },
+    "scroll-down": () => window.scrollByLines(1),
+    "scroll-up": () => window.scrollByLines(-1),
+    "scroll-top": () => window.scroll(0, 0),
+    "scroll-bottom": () => window.scroll(0, document.body.scrollHeight),
+    "go-back": () => history.back(),
+    "go-forward": () => history.forward(),
+    "reload-page": () => location.reload()
   }
 
   let ws = new WebSocket("ws://localhost:9999");
